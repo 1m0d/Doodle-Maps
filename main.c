8c4c924b3c67f47d1847b43824b178c02d3e7ba0
1m0d <domi@shinrai.co>
2017-11-11 17:49:56 +0100

    init create_graph()

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
 #include "cJSON.h"
 
 typedef struct{
@@ -17,6 +18,89 @@ typedef struct{
   int hexagon_height;
   int draw_area[2];
 }ParsedMap;
+
+typedef struct{
+  int index_of_nodes[2];
+  int weight;
+}Edge;
+
+typedef struct{
+  int distance_to_end;
+  struct Edge **edges;
+}Node;
+
+typedef struct{
+  Node *nodes;
+  int size;
+}Graph;
+
+//TODO fix
+int **get_neighbours(ParsedMap map, int *neighbour_count){
+  int even_row_offset[6][2] = {{-1, -1}, {-1, 0}, {0, 1}, {0, -1}, {1, 0}, {1, -1}};
+  int odd_row_offset[6][2] = {{-1, 0}, {-1, 1}, {0, 1}, {0, -1}, {1, 1}, {1, 0}};
+  bool memory_allocated = false;
+  int neighbours_added = 0;
+  int **neighbours;
+
+  while(true){
+    for(int tile = 0; tile < map.tile_count; tile++){
+      for (int adjacent_tile = 0; adjacent_tile < 6; adjacent_tile++){
+        int coo2check[2];
+        for(int i = 0; i < 2; i++){
+          if(map.tiles[tile].coordinates[0] % 2 == 0)
+            coo2check[i] = map.tiles[tile].coordinates[i] + even_row_offset[adjacent_tile][i];
+          else
+            coo2check[i] = map.tiles[tile].coordinates[i] + odd_row_offset[adjacent_tile][i];
+        }
+        if(coo2check[0] >= 0 && coo2check[1] >= 0 && get_tile_weight(map, coo2check) != 0){
+          if(tile < coo2index(map, coo2check)){  //don't create duplicate data of same neighbours
+            if(memory_allocated){
+              neighbours[neighbours_added][0] = tile;
+              neighbours[neighbours_added][1] = coo2index(map, coo2check);
+              neighbours_added++;
+            }
+            else{
+              (*neighbour_count)++;
+            }
+          }
+        }
+      }
+    }
+
+      if(!memory_allocated){
+        neighbours = (int **) malloc(*neighbour_count * sizeof(int) * 2);
+        for(int i = 0; i < *neighbour_count; i++){
+          neighbours[i] = (int *) malloc(2 * sizeof(int));
+        }
+        memory_allocated = true;
+      }
+      if(neighbours_added == *neighbour_count)
+        break;
+  }
+  /*countPtr = &neighbour_count;*/
+  return neighbours;
+}
+
+Graph create_graph(ParsedMap map){
+  Graph graph;
+  graph.size = map.tile_count;
+  int edge_count = 0;
+  int *edge_countPtr = &edge_count;
+  int **neighbours = get_neighbours(map, edge_countPtr);
+
+  Node nodes[graph.size];
+  Edge edges[edge_count];
+
+  //create edges
+  for(int i = 0; i < edge_count; i++){
+    edges[i].index_of_nodes[0] = neighbours[i][0];
+    edges[i].index_of_nodes[1] = neighbours[i][1];
+    edges[i].weight = get_tile_weight(map, index2coo(map, neighbours[i][0])) + get_tile_weight(map, index2coo(map, neighbours[i][1]));
+  }
+
+  return graph;
+}
+
 
 char *file_to_string(FILE *file){
   char *file_contents;
@@ -187,6 +271,7 @@ int main(int argc, char *argv[]){
 
   char *map_json = file_to_string(map_file);
   ParsedMap parsed_map = parse_map(map_json);
+  Graph graph = create_graph(parsed_map);
   draw_map(parsed_map);
   //map_parsed = parse_json(map_json)
   //map_graph = graph(map_parsed)
